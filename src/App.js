2f8a958d5240015721dcc50978b21735bb75eb6a
Prashanth Sobbani <sobbani.prashanth@gmail.com>
2020-04-26 23:16:43 -0400

    Added Particles.js to the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import FunctionalComponent from './FunctionalComponent/FunctionalComponent';
-import ClassComponent,  { ClassComponent2 as Div } from './ClassComponent/ClassComponent';
+import ClassComponent, { ClassComponent2 as Div } from './ClassComponent/ClassComponent';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!!!</h1>
-      <FunctionalComponent></FunctionalComponent>
-      <ClassComponent></ClassComponent>
-      <Div></Div>
+      <h1 style={{ color: "#ffffff" }}>Prashanth Sobbani!!!</h1>
     </div>
   );
 }
